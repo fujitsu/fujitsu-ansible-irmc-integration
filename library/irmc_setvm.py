e5a2b8d4c4b2916bb441d3baf0c7f9c47b02b6d7
nakai.tomohisa <nakai.tomohisa@fujitsu.com>
2024-08-21 17:48:16 +0900

    fix: delete unused choices

--- library/irmc_setvm.py
+++ library/irmc_setvm.py
@@ -209,7 +209,7 @@ def main():
         irmc_username=dict(required=True, type='str'),
         irmc_password=dict(required=True, type='str', no_log=True),
         validate_certs=dict(required=False, type='bool', default=True),
-        vm_type=dict(required=False, type='str', default='CDImage', choices=['CDImage', 'FDImage', 'HDImage']),
+        vm_type=dict(required=False, type='str', default='CDImage', choices=['CDImage', 'HDImage']),
         server=dict(required=True, type='str'),
         share=dict(required=True, type='str'),
         image=dict(required=True, type='str'),
